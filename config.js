66ada8613d16bcea79e6d79832fa0f9eb0e44ca7
Nipun malinda <129479822+Nipunmalinda@users.noreply.github.com>
2024-12-02 14:07:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aNMEkbQB#MOr4cPGN_UCJmIjqfkDS2TNxi3hC7HOD9Q_21RInPzs",
+SESSION_ID: process.env.SESSION_ID || "2Z8iGJwT#6oyqifuz-NoP6RH55yHd3kv1cxZaU3m5B_5rKUXnlIw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
